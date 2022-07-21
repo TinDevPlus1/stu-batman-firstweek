67c0c934caff392cf832cd606a87e47ae9b077a1
Thanh-Tinh <tinh.pham@devplus.edu.vn>
2022-07-21 16:45:03 +0700

    BM-10 - footer

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,14 +1,18 @@
-$(document).ready(function (e) {
-    //click
-    $(".scrollUp").click(function (e) {
-      //stop
-      e.preventDefault();
-      //get header href
-      var url = $(this).attr("href");
-      //offset
-      var offset = $(url).offset();
-      //scroll top
-      $("html, body").animate({ scrollTop: offset.top });
-    });
-  });
-  
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
